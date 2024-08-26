e354fd414ef6e48bab93f1e168fa93265a1a95b0
Rezwan Chowdhury <tipu.diu.cse@gmail.com>
2024-08-26 10:18:53 +0600

    intial commint
    intial commint

---
+++ Face_APP/Face_APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Face_APP
+//
+//  Created by Rezwan Chowdhury on 26/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Face_APP/Face_APP/Face_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_APPApp.swift
+//  Face_APP
+//
+//  Created by Rezwan Chowdhury on 26/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Face_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
